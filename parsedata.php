0a9bbb7a59cfc79e74f15da72e6735b6f1f668c5
Marcus Brohede <a97marbr@gmail.com>
2017-10-03 12:48:02 +0200

    added ajax transmitting of benchmark

---
+++ parsedata.php
@@ -0,0 +1,44 @@
+<?php
+
+//        (\ /)
+//        (. .)           
+//       c(”)(”)    . 
+//---------------------
+// CREATE TABLE gdata(xres integer, yres integer, tracingtime real, vectortime real, objno integer, pointno integer, vectno integer, updtime datetime, url text, did INTEGER AUTO_INCREMENT, filenme varchar(128), primary key(did));
+
+date_default_timezone_set("Europe/Berlin");
+
+$datan = $_POST['benchmark'];
+$dataarr=explode("\n", $datan);
+
+$pdo = new PDO('mysql:dbname=eurographicsdata;host=localhost', 'wikiadmin', 'kingfisher');
+$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+/*
+$sql = 'DELETE FROM gdata WHERE filenme=:filenme);';
+$stmt = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+$stmt->bindParam(':filenme', $datastr);
+*/
+foreach($dataarr as $key => $value){
+    $dataarrz=explode(",", $value);
+  print_r($dataarrz);
+    if(count($dataarrz)>5){
+        $sql = 'INSERT INTO gdata(xres,yres,tracingtime,vectortime, objno, pointno, vectno, updtime, url,filenme) VALUES(:xres,:yres,:tracingtime,:vectortime,:objno,:pointno,:vectno,:updtime,:url,:filenme);';
+        $stmt = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+        $stmt->bindParam(':xres', $dataarrz[0]);
+        $stmt->bindParam(':yres', $dataarrz[1]);
+        $stmt->bindParam(':tracingtime', floatval($dataarrz[2]));
+        $stmt->bindParam(':vectortime', floatval($dataarrz[3]));
+        $stmt->bindParam(':objno', floatval($dataarrz[4]));
+        $stmt->bindParam(':pointno', floatval($dataarrz[5]));
+        $stmt->bindParam(':vectno', floatval($dataarrz[6]));
+        $stmt->bindParam(':updtime', date("Y-m-d H:i:s",strtotime($dataarrz[7])));
+        $stmt->bindParam(':url', $dataarrz[8]);
+        $stmt->bindParam(':filenme', $datastr);
+        $stmt->execute();
+    }
+      
+}
+
+//print_r($dataarr);
+
+?>
