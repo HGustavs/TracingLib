d5410970ed59fff496b3869aa84812a574a9ea98
Marcus Brohede <a97marbr@gmail.com>
2017-10-03 13:01:58 +0200

    Now with series

--- parsedata.php
+++ parsedata.php
@@ -9,15 +9,17 @@
 date_default_timezone_set("Europe/Berlin");
 
 $datan = $_POST['benchmark'];
+$filename = $_POST['m']
+
 $dataarr=explode("\n", $datan);
 
 $pdo = new PDO('mysql:dbname=eurographicsdata;host=localhost', 'wikiadmin', 'kingfisher');
 $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
-/*
+
 $sql = 'DELETE FROM gdata WHERE filenme=:filenme);';
 $stmt = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
-$stmt->bindParam(':filenme', $datastr);
-*/
+$stmt->bindParam(':filenme', $filename);
+
 foreach($dataarr as $key => $value){
     $dataarrz=explode(",", $value);
   print_r($dataarrz);
